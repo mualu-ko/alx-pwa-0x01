572fc74db7f07a51f773da832398c80ae3b62a64
mualu-ko <kmwaluko.wambua@gmail.com>
2025-10-26 22:14:09 +0300

    start

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,3 +7,21 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
+};
+
+export default withPWA({
+  ...nextConfig
+})
